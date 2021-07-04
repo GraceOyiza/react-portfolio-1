d31fc670a0f9f3e6a96da1d24769b41cf2d3d975
GraceOyiza <graceoyiza13@gmail.com>
2021-07-04 22:55:17 +0100

    Add github and live demo link

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,43 +1,77 @@
 import React, { Component } from 'react';
 
 class Portfolio extends Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e) {
+    e.preventDefault();
+    window.open(e.target.parentElement.href);
+  }
   render() {
-
-    if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="portfolio-item" style={{marginBottom: '30px'}}>
-           <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
+    if (this.props.data) {
+      var projects = this.props.data.projects.map((projects, index) => {
+        var projectImage = 'images/portfolio/' + projects.image;
+        return (
+          <div
+            key={projects.title + index}
+            className="portfolio-item"
+            style={{ marginBottom: '30px' }}
+          >
+            <div className="item-wrap">
+              <a
+                href={projects.url}
+                title={projects.title}
+                onClick={this.handleClick}
+              >
+                <img alt={projects.title} src={projectImage} />
+                <div className="overlay">
                   <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
+                    <h5>{projects.title}</h5>
+                    <p>{projects.category}</p>
                   </div>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
+                <div className="link-icon">
+                  <a
+                    href={projects.url}
+                    title={projects.title}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fa fa-link"></i>
+                  </a>
+                  <a
+                    href={projects.repo}
+                    title={projects.title}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fab fa-github"></i>
+                  </a>
+                </div>
+              </a>
+            </div>
           </div>
-        </div>
-      })
+        );
+      });
     }
 
     return (
       <section id="portfolio">
-
-      <div className="row">
-
-         <div className="twelve columns collapsed">
-
+        <div className="row">
+          <div className="twelve columns collapsed">
             <h1>Check Out Some of My Works.</h1>
 
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {projects}
+            <div
+              id="portfolio-wrapper"
+              className="bgrid-quarters s-bgrid-thirds cf"
+            >
+              {projects}
             </div>
           </div>
-      </div>
-   </section>
+        </div>
+      </section>
     );
   }
 }
